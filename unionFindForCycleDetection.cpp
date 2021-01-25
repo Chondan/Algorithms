d634aef3573366f37885f31af5bf2f9132090290
Chondan Susuwan <chondansusuwan@outlook.com>
2021-01-25 18:22:03 +0700

    added union find for cycle detection

--- unionFindForCycleDetection.cpp
+++ unionFindForCycleDetection.cpp
@@ -1,11 +1,12 @@
 #include <iostream> 
+#include <unordered_map>
 #include <vector> // Vectors are same as dynamic arrays with the ability to resize itself automatically.
 using namespace std;
 
 // data structure to store graph edges
 struct Edge {
 	int src, dest;
-}
+};
 
 // class to represnet a graph object
 class Graph {
@@ -20,12 +21,90 @@ public:
 		// resize the vector to N elements of type vector<int>
 		adjList.resize(N + 1);
 
+		// add edges to the graph
 		for (auto &edge: edges) {
 			adjList[edge.src].push_back(edge.dest);
 		}
 	}
+};
+
+// class main to represent a disjoint set
+class Main {
+	unordered_map<int, int> parent;
+public:
+	// perform Makeset operation
+	void makeSet(int N) {
+		// create N disjoint sets (one for each vertex)
+		for (int i = 1; i <= N; i++) {
+			parent[i] = i;
+		}
+	}
+
+	// Find the root of the set in which element k belongs
+	int Find(int k) {
+		// if k is root
+		if (parent[k] == k) return k;
+
+		// recur for parent until we find root 
+		return Find(parent[k]);
+	}
+
+	// Perform Union of two subsets 
+	void Union(int a, int b) {
+		// find root of the sets in which elements x and y belongs
+		int x = Find(a);
+		int y = Find(b);
+		parent[x] = y;
+	}
+};
+
+bool findCycle(Graph const &graph, int N) {
+	// initialize Main class
+	Main ds;
+
+	// create singleton set for each element of universe
+	ds.makeSet(N);
+
+	// consider every edge (u, v)
+	for (int u = 1; u <= N; u++) {
+		// Recur for all adjacent vertices
+		for (int v : graph.adjList[u]) {
+			// find root of the sets to which elements u and v belongs
+			int x = ds.Find(u);
+			int y = ds.Find(v);
+
+			// both u and v have same parent, cycle is found
+			if (x == y) return true;
+			else ds.Union(x, y);
+		}
+	}
+	return false;
 }
+
+// Complete Algorithm
+// 1. Create a disjoint sets for each vertex of the graph.
+// 2. For every edge u, v in the graph
+//   i) Find root of the sets to which elements u and v belongs
+//  ii) If both u and v have same root is disjoint sets, a cycle is found.
+
 
 int main(void) {
 
+	// vector of graph edges as per above diagram
+	vector<Edge> edges = {
+		{1, 2}, {1, 7}, { 1, 8}, {2, 3}, {2, 6}, {3, 4},
+		{3, 5}, {8, 9}, {8, 12}, {9, 10}, {9, 11}, {11, 12}
+		// edge (11, 12) introduces a cycle in the graph
+	};
+
+	// Number of nodes in the graph
+	int N = 12;
+
+	// create a graph from edges
+	Graph graph(edges, N);
+
+	if (findCycle(graph, N)) cout << "Cycle Found" << endl;
+	else cout << "No Cycle Found" << endl;
+
 }
+
